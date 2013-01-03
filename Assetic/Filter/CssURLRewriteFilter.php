b0a79551e42077a31647bf7d8a4a27edd185d6ea
Marek Kalnik <marekk@theodo.fr>
2013-01-03 13:15:01 +0100

    Fix indentation

--- Assetic/Filter/CssURLRewriteFilter.php
+++ Assetic/Filter/CssURLRewriteFilter.php
@@ -1,155 +1,154 @@
 <?php
-    namespace Fkr\CssURLRewriteBundle\Assetic\Filter;
+namespace Fkr\CssURLRewriteBundle\Assetic\Filter;
 
-    use Symfony\Component\HttpKernel\KernelInterface;
-    use Symfony\Component\DependencyInjection\Exception\InactiveScopeException;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\DependencyInjection\Exception\InactiveScopeException;
 
-    use Assetic\Asset\AssetInterface;
-    use Assetic\Filter\FilterInterface;
+use Assetic\Asset\AssetInterface;
+use Assetic\Filter\FilterInterface;
 
-    class CssURLRewriteFilter implements FilterInterface
+class CssURLRewriteFilter implements FilterInterface
+{
+    private $rewriteIfFileExists;
+    private $clearUrls;
+    private $kernel;
+
+    private $asset;
+
+    public function __construct($rewriteIfFileExists, $clearUrls, KernelInterface $kernel)
     {
-        private $rewriteIfFileExists;
-        private $clearUrls;
-        private $kernel;
+        $this->rewriteIfFileExists = $rewriteIfFileExists;
+        $this->clearUrls = $clearUrls;
+        $this->kernel = $kernel;
+    }
 
-        private $asset;
+    public function filterLoad(AssetInterface $asset)
+    {
+    }
 
-        public function __construct($rewriteIfFileExists, $clearUrls, KernelInterface $kernel)
-        {
-            $this->rewriteIfFileExists = $rewriteIfFileExists;
-            $this->clearUrls = $clearUrls;
-            $this->kernel = $kernel;
+    public function filterDump(AssetInterface $asset)
+    {
+        $this->asset = $asset;
+        $bundlePath = $this->calculateBundlePath();
+        if ($bundlePath === null) {
+            return;
         }
 
-        public function filterLoad(AssetInterface $asset)
-        {
-        }
+        $that = $this;
 
-        public function filterDump(AssetInterface $asset)
-        {
-            $this->asset = $asset;
-            $bundlePath = $this->calculateBundlePath();
-            if ($bundlePath === null) {
-                return;
+        $content = $asset->getContent();
+        $content = preg_replace_callback('|(url)\((["\']?)([^\)]+)\)|i', function($matches) use ($that, $bundlePath) {
+            $tmpPath = $that->checkForBundleLinking($matches[3]);
+            if ($tmpPath != null) {
+                return $matches[1].'('.$matches[2].$tmpPath.')';
+            } elseif (!$that->checkPath($matches[3])) {
+                return $matches[1].'('.$matches[2].$matches[3].')';
             }
 
-            $that = $this;
+            return $matches[1].'('.$matches[2].$that->normalizeUrl($bundlePath.'/'.$matches[3]).')';
+        }, $content);
+        $asset->setContent($content);
+    }
 
-            $content = $asset->getContent();
-            $content = preg_replace_callback('|(url)\((["\']?)([^\)]+)\)|i', function($matches) use ($that, $bundlePath) {
-                $tmpPath = $that->checkForBundleLinking($matches[3]);
-                if ($tmpPath != null) {
-                    return $matches[1].'('.$matches[2].$tmpPath.')';
-                } elseif (!$that->checkPath($matches[3])) {
-                    return $matches[1].'('.$matches[2].$matches[3].')';
-                }
+    public function checkForBundleLinking($path)
+    {
+        if (substr($path, 0, 1) == '@') {
+            $findChar = strpos($path, '/');
+            $bundleName = substr($path, 1, $findChar-1);
+            try {
+                $bundle = $this->kernel->getBundle($bundleName);
 
-                return $matches[1].'('.$matches[2].$that->normalizeUrl($bundlePath.'/'.$matches[3]).')';
-            }, $content);
-            $asset->setContent($content);
-        }
-
-        public function checkForBundleLinking($path)
-        {
-            if (substr($path, 0, 1) == '@') {
-                $findChar = strpos($path, '/');
-                $bundleName = substr($path, 1, $findChar-1);
-                try {
-                    $bundle = $this->kernel->getBundle($bundleName);
-
-                    return $this->calculateSwitchPath().'bundles/'.str_replace('_', '', $bundle->getContainerExtension()->getAlias()).substr($path, $findChar);
-                } catch (\InvalidArgumentException $e) {
-                }
+                return $this->calculateSwitchPath().'bundles/'.str_replace('_', '', $bundle->getContainerExtension()->getAlias()).substr($path, $findChar);
+            } catch (\InvalidArgumentException $e) {
             }
-
-            return null;
         }
 
-        private function calculateBundlePath()
-        {
-            foreach ($this->kernel->getBundles() as $bundle) {
-                if (strstr($this->asset->getSourceRoot(), $bundle->getPath()) !== false) {
-                    if ($bundle->getContainerExtension()) {
-                        return $this->calculateSwitchPath().'bundles/'.str_replace('_', '', $bundle->getContainerExtension()->getAlias()).'/'.$this->calculatePathSuffix();
-                    }
+        return null;
+    }
+
+    private function calculateBundlePath()
+    {
+        foreach ($this->kernel->getBundles() as $bundle) {
+            if (strstr($this->asset->getSourceRoot(), $bundle->getPath()) !== false) {
+                if ($bundle->getContainerExtension()) {
+                    return $this->calculateSwitchPath().'bundles/'.str_replace('_', '', $bundle->getContainerExtension()->getAlias()).'/'.$this->calculatePathSuffix();
                 }
             }
-
-            return null;
         }
 
-        private function calculatePathSuffix()
-        {
-            $sourcePath = dirname($this->asset->getSourcePath());
+        return null;
+    }
 
-            return $this->getSubstringAfterNString($sourcePath, '/', 2);
+    private function calculatePathSuffix()
+    {
+        $sourcePath = dirname($this->asset->getSourcePath());
+
+        return $this->getSubstringAfterNString($sourcePath, '/', 2);
+    }
+
+    private function getSubstringAfterNString($string, $searchString, $afterN)
+    {
+        $i=0;
+        $pos = -1;
+        while (($pos=strpos($string, $searchString, $pos+1)) !== false) {
+            $i++;
+
+            if ($i==$afterN) {
+                return substr($string, $pos+1);
+            }
         }
 
-        private function getSubstringAfterNString($string, $searchString, $afterN)
-        {
-            $i=0;
-            $pos = -1;
-            while (($pos=strpos($string, $searchString, $pos+1)) !== false) {
-                $i++;
+        return '';
+    }
 
-                if ($i==$afterN) {
-                    return substr($string, $pos+1);
+    private function calculateSwitchPath()
+    {
+        $targetPath = dirname($this->asset->getTargetPath());
+        $numDirs = substr_count($targetPath, '/')+1;
+
+        $output = '';
+        for ($i=0; $i<$numDirs; $i++) {
+            $output .= '../';
+        }
+
+        if (substr($targetPath, 0, 11) == '_controller') {
+            try {
+                $request = $this->kernel->getContainer()->get('request');
+                if (substr($request->getBaseUrl(), -4) != '.php') {
+                    $output = substr($output, 3);
                 }
+            } catch (InactiveScopeException $e) {
             }
-
-            return '';
         }
 
-        private function calculateSwitchPath()
-        {
-            $targetPath = dirname($this->asset->getTargetPath());
-            $numDirs = substr_count($targetPath, '/')+1;
+        return $output;
+    }
 
-            $output = '';
-            for ($i=0; $i<$numDirs; $i++) {
-                $output .= '../';
-            }
-
-            if (substr($targetPath, 0, 11) == '_controller') {
-                try {
-                    $request = $this->kernel->getContainer()->get('request');
-                    if (substr($request->getBaseUrl(), -4) != '.php') {
-                        $output = substr($output, 3);
-                    }
-                } catch (InactiveScopeException $e) {
-                }
-            }
-
-            return $output;
+    public function checkPath($url)
+    {
+        if (!$this->rewriteIfFileExists) {
+            return true;
         }
 
-        public function checkPath($url)
-        {
-            if (!$this->rewriteIfFileExists) {
-                return true;
-            }
-
-            $lastChar = substr($url, -1);
-            if ($lastChar=='"' || $lastChar=='\'') {
-                $url = substr($url, 0, -1);
-            }
-
-            return file_exists($this->asset->getSourceRoot().'/'.dirname($this->asset->getSourcePath()).'/'.$url);
+        $lastChar = substr($url, -1);
+        if ($lastChar=='"' || $lastChar=='\'') {
+            $url = substr($url, 0, -1);
         }
 
-        public function normalizeUrl($url)
-        {
-            if (!$this->clearUrls) {
-                return $url;
-            }
+        return file_exists($this->asset->getSourceRoot().'/'.dirname($this->asset->getSourcePath()).'/'.$url);
+    }
 
-            $pattern = '/\w+\/\.\.\//';
-            while (preg_match($pattern, $url)) {
-                $url = preg_replace($pattern, '', $url);
-            }
-
+    public function normalizeUrl($url)
+    {
+        if (!$this->clearUrls) {
             return $url;
         }
 
+        $pattern = '/\w+\/\.\.\//';
+        while (preg_match($pattern, $url)) {
+            $url = preg_replace($pattern, '', $url);
+        }
+
+        return $url;
     }
+}
